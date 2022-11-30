f0dbc33c79e671dfd07dce697194bf75cc5e4189
euclidesseg <euder0206@gmail.com>
2022-11-30 17:42:53 -0500

    comisiones

---
+++ SistemaVentasFB/app/src/main/java/com/example/sistemaventasfb/sales.java
@@ -0,0 +1,31 @@
+package com.example.sistemaventasfb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class sales extends AppCompatActivity {
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    String idAutomatic;
+    double mTotcomision = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sales);
+        EditText idsellers = findViewById(R.id.etidsellersale);
+        EditText idsale = findViewById(R.id.etidsale);
+        EditText datesale = findViewById(R.id.etdatesale);
+        EditText salevalue = findViewById(R.id.etsalevalue);
+        ImageButton btnsalevalue = findViewById(R.id.btnsavesale);
+        ImageButton btnback = findViewById(R.id.btnback);
+
+
+    }
+}
+
